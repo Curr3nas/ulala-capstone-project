6e8f846b4f19f27ec9bdab5a544495380b9ec0e7
Douglas Miner <curr3nas@gmail.com>
2020-04-06 00:25:19 -0500

    added media and skills array for shaman and assassin

---
+++ skills/shaman-skills.js
@@ -0,0 +1,134 @@
+let shamanSkills = [
+  {
+    name: "Rain Dance",
+    rarity: "Normal",
+    type: "Cast",
+    description: "Dance in the rain to heal the teammate with the lowest HP for 27%(+52) of ATK"
+  },
+  {
+    name: "Riptide",
+    rarity: "Normal",
+    type: "Instant",
+    description: "Heal the teammate with the lowest HP for 14%(+27) of ATK, and another same amount of HP over 5 seconds"
+  },
+  {
+    name: "Spring Totem",
+    rarity: "Normal",
+    type: "Instant",
+    description: "Summon 1 Spring Totem to heal the 2 teammates with the lowest HP for 17%(+32) of ATK over 5 seconds"
+  },
+  {
+    name: "Drums of Purge",
+    rarity: "Normal",
+    type: "Cast",
+    description: "Purify the teammate with the lowest HP. Heal them for 22%(+42) of ATK and sheild them from any debuff for 3 seconds"
+  },
+  {
+    name: "Electric Shock",
+    rarity: "Normal",
+    type: "Cast",
+    description: "Heal the teammate with the lowest HP for 24%(+46) of ATK and cast an electric current to travel back and forth between enemy units 5 times, each time inflicting 24%(+46) ATK DMG"
+  },
+  {
+    name: "Ancestral Protection",
+    rarity: "Normal",
+    type: "Instant",
+    description: "Call for Ancestral Protection to increase healing by 10%. Heal the teammate with the lowest HP for 16.2%(+31) of ATK"
+  },
+  {
+    name: "Faith Protection",
+    rarity: "Normal",
+    type: "Cast",
+    description: "Heal the teammate with the lowest HP for 22%(+42) of ATK. If in the Ancestral Protection state, provide the target with extra protection for 10 seconds and heal them for another 40% of the HP restored when they are attacked the next time"
+  },
+  {
+    name: "Dance With Me",
+    rarity: "Normal",
+    type: "Cast",
+    description: "Healself and the teammate with the lowest HP for 19%(+37) of ATK"
+  },
+  {
+    name: "Thunder Heart",
+    rarity: "Normal",
+    type: "Instant",
+    description: "Gain the Thunder Force and inflict 14%(+26) ATK DMG on 3 enemy units upon the next 3 basic attacks. Heal the teammate with the lowest hp for 4%(+8) of ATK"
+  },
+  {
+    name: "Drums of Recovery",
+    rarity: "Normal",
+    type: "Cast",
+    description: "Play the drums and heal the teammate with the lowest HP for 20%(+38) of ATK. The lower the teammates HP, the higher the healing"
+  },
+  {
+    name: "Drums of Rain",
+    rarity: "Normal",
+    type: "Cast",
+    description: "Heal the 2 teammates with the lowest HP for 18.9%(+37) of ATK"
+  },
+  {
+    name: "Ancestral Gift",
+    rarity: "Normal",
+    type: "Cast",
+    description: "Pray for the Ancestor's Strength to heal the party for 10%(+20) of ATK and gain 1 Ancestral Mark"
+  },
+  {
+    name: "Chain Heal",
+    rarity: "Normal",
+    type: "Cast",
+    description: "Release the Ancestral Mark to heal the party for 13%(+25) of ATK. Increase healing by 25% for each Ancestral Mark you own"
+  },
+  {
+    name: "Blesing of Spirits",
+    rarity: "Normal",
+    type: "Instant",
+    description: "Pray for the Ancestor's Strength to heal the party for 11%(+21) of ATK and obtain 1 Ancestral Mark. For those who already own the Ancestral Mark, healing increases by 10%"
+  },
+  {
+    name: "Font of Purge",
+    rarity: "Normal",
+    type: "Cast",
+    description: "Remove 1 debuff from all party members and heal 15%(+29) of ATK. Increase the HP recovered by 10% for each debuff removed"
+  },
+  {
+    name: "Recovery Totem",
+    rarity: "Normal",
+    type: "Instant",
+    description: "Summon 1 Recovery Totem to heal the entire party for 9.5%(+18) of ATK over 5 seconds"
+  },
+  {
+    name: "Life Totem",
+    rarity: "Normal",
+    type: "Instant",
+    description: "Summon a Life Totem with HP of 23%(+45) of ATK. Links to the teammate with the lowest HP to transfer 80% of their DMG received to the totem for 10 seconds"
+  },
+  {
+    name: "Surge",
+    rarity: "Normal",
+    type: "Instant",
+    description: "Use the Life Force and heal the 2 teammates with the lowest HP for 13%(+25) of ATK. Shorten the next skills casting time by 40%"
+  },
+  {
+    name: "Ancestral Aegis",
+    rarity: "Normal",
+    type: "Instant",
+    description: "Protect the teammate with the lowest HP. Within 10 seconds, heal 3%(+6) of ATK for each basic attack it receives. Can be triggered up to 15 times"
+  },
+  {
+    name: "",
+    rarity: "Normal",
+    type: "Cast",
+    description: ""
+  },
+  {
+    name: "Fury of the Ancestors",
+    rarity: "Normal",
+    type: "Instant",
+    description: "Inflict 21%(+41) of ATK as DMG on all enemy units and lower the enemy's healing by 15%"
+  },
+  {
+    name: "Charm Dance",
+    rarity: "Normal",
+    type: "Cast",
+    description: "Inflict 8.8%(+17) ATK DMG on all enemy units and silence the targets for 1 second (The Silence effect has a 100% hit rate for enemy units at Lv10 or lower"
+  }
+];
