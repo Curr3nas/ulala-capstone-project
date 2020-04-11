e8f292ab6bd556c4a08762ab5a2ccacffc0dec9f
Douglas Miner <curr3nas@gmail.com>
2020-04-10 20:57:17 -0500

    finished glad skills

--- skills/glad-skills.js
+++ skills/glad-skills.js
@@ -36,9 +36,87 @@ let gladSkills = [
     description: "Taunt all the enemies to attack you and restore 2.7%(+12) HP over 10 seconds (The Taunt effect has a 100% hit rate for targets at Lv10 or lower)"
   },
   {
-    name: "",
+    name: "Barbarian Veins",
     rarity: "Normal",
     type: false,
-    description: ""
+    description: "Activate Barbarian Veins to raise Max HP by 10% for 8 seconds and restore HP of 4.4%(+20) over 10 seconds"
+  },
+  {
+    name: "Blood Force",
+    rarity: "Normal",
+    type: false,
+    description: "Increase the Healing received by 10% and restore 4.6%(+21) HP over 10 seconds. If in the Barbarian Veins state, increase healing by 50%"
+  },
+  {
+    name: "Intercept",
+    rarity: "Normal",
+    type: false,
+    description: "Inflict 70%(+70) ATK DMG on an enemy unit and interrupt its skill casting. Successful interruption restores 2.7%(+12) HP and reduces DMG received from the enemy by 8% for 10 seconds (The Interrupted effect has a 100% hit rate for enemy units at Lv10 or lower)"
+  },
+  {
+    name: "Sacrifice",
+    rarity: "Normal",
+    type: false,
+    description: "Restore 3.1%(+14) HP over 10 seconds and take 40% of the DMG received every time for the teammate with the lowest HP"
+  },
+  {
+    name: "Rising Rage",
+    rarity: "Normal",
+    type: false,
+    description: "Immune to control effects for 16 seconds and restore 4%(+18) HP over 10 seconds"
+  },
+  {
+    name: "Hoo Hoo Hoo",
+    rarity: "Normal",
+    type: false,
+    description: "Reduce DMG taken by other teammates by 16% over 10 seconds. Up to 2.5%(+11) of your HP can be reduced"
+  },
+  {
+    name: "Death Denied",
+    rarity: "Normal",
+    type: false,
+    description: "Restore 4.1%(+19) HP over 5 seconds. The lower the HP the higher the healing"
+  },
+  {
+    name: "Uppercut",
+    rarity: "Normal",
+    type: "Charge",
+    description: "Attack the enemy with an uppercut, inflicting 60%(+60) ATK DMG, and knock the target into the air (This effect has a 100% hit rate for enemy units at Lv10 or lower"
+  },
+  {
+    name: "Battle Shout",
+    rarity: "Normal",
+    type: false,
+    description: "Remove 1 debuffs for yourself and restore 4%(+18) HP over 10 seconds"
+  },
+  {
+    name: "Let's charge",
+    rarity: "Normal",
+    type: false,
+    description: "Inspire your party and increase ATK by 13%(+13) for 6 seconds"
+  },
+  {
+    name: "Bloody Combat",
+    rarity: "Normal",
+    type: "Charge",
+    description: "Restore 3.1%(+14) HP over 10 seconds, during which recover 1 point of energy if the damage taken is more than 25% of HP"
+  },
+  {
+    name: "Severing Strike",
+    rarity: "Normal",
+    type: false,
+    description: "Consume 20% of your current HP to inflict 105%(+105) ATK DMG to enemy units in the back row. If the targets are killed within 3 seconds, restore 2.7%(+12) of your HP. If in the frenzied state, DMG increases by 50%"
+  },
+  {
+    name: "Frenzy",
+    rarity: "Normal",
+    type: false,
+    description: "Enter the Frenzied state to increase ATK by 30%, at the cost of 30% Max HP. Inflict 184%(+184) ATK DMG on the enemy with the lowest HP, and lower its healing by 16%. In the Frenzied state, DMG increases by 50%"
+  },
+  {
+    name: "Fevered Pursuit",
+    rarity: "Normal",
+    type: "Charge",
+    description: "Attack 3 times and inflict 13%(+13) ATK DMG. If in the Frenzied state, attack range increases and attacks focus on the enemy unit with the lowest HP, stunning the target for 0.5 seconds (The Stun effect has a 100% hit rate for enemy units at Lv10 or lower)"
   },
 ]
