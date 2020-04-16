71498a77bb80b66eb689d8e28c35092049cbdc98
Douglas Miner <curr3nas@gmail.com>
2020-04-16 12:55:44 -0500

    added attributes

---
+++ src/attributes/ClassAttributes.js
@@ -0,0 +1,229 @@
+export let attributes = [
+  {
+    name: "Agility",
+    desc: "Agility affects the ATK, HIT and Impale stats.",
+    statOne: "ATK affects the character's ATK DMG.",
+    statTwo: "HIT affects the character's Hit Rate (while confronting an enemy of your level, the primary hit rate is 95%)",
+    statThree: "Impale lowers the chance of your atttack being blocked and increases Crit DMG (while confronting an enemy of your level",
+  },
+  {
+    name: "Sta",
+    desc: "Stamina affects the HP, Block and Evasion stats.",
+    statOne: "HP affects the character's Max HP. At 0 HP, the character dies.",
+    statTwo: "Block affects the character's Block Chance. DMG received is reduced when an attack is blocked (while confronting an enemy of your level)",
+    statThree: "Evasion affects the character's Evasion Rate. No damage is taken when an attack is evaded (while confronting an enemy of your level)"
+  },
+  {
+    name: "Tech",
+    desc: "Tech affects the Armor, Crit and Tenacity stats.",
+    statOne: "Armor reduces the character's damage taken.",
+    statTwo: "Crit affects the character's Crit Rate. DMG increases when you Crit. (while confronting an enemy of your level)",
+    statThree: "Tenacity lowers the chance of being Critically hit and Crit. Dmg taken (while confronting an enemy of your level)",
+  },
+  {
+    name: "Str",
+    desc: "Str affects the ATK, HIT and Impale stats.",
+    statOne: "ATK affects the character's ATK DMG.",
+    statTwo: "HIT affects the character's Hit Rate (while confronting an enemy of your level, the primary hit rate is 95%)",
+    statThree: "Impale lowers the chance of your atttack being blocked and increases Crit DMG (while confronting an enemy of your level",
+  },
+  {
+    name: "Int",
+    desc: "Int affects the ATK, HIT and Impale stats.",
+    statOne: "ATK affects the character's ATK DMG.",
+    statTwo: "HIT affects the character's Hit Rate (while confronting an enemy of your level, the primary hit rate is 95%)",
+    statThree: "Impale lowers the chance of your atttack being blocked and increases Crit DMG (while confronting an enemy of your level",
+  }
+]
+
+export let assassinAttributes = [
+  {
+    name: "Agility",
+    desc: "Agility affects the ATK, HIT and Impale stats.",
+    statOne: "ATK affects the character's ATK DMG.",
+    statTwo: "HIT affects the character's Hit Rate (while confronting an enemy of your level, the primary hit rate is 95%)",
+    statThree: "Impale lowers the chance of your atttack being blocked and increases Crit DMG (while confronting an enemy of your level",
+  },
+  {
+    name: "Sta",
+    desc: "Stamina affects the HP, Block and Evasion stats.",
+    statOne: "HP affects the character's Max HP. At 0 HP, the character dies.",
+    statTwo: "Block affects the character's Block Chance. DMG received is reduced when an attack is blocked (while confronting an enemy of your level)",
+    statThree: "Evasion affects the character's Evasion Rate. No damage is taken when an attack is evaded (while confronting an enemy of your level)"
+  },
+  {
+    name: "Tech",
+    desc: "Tech affects the Armor, Crit and Tenacity stats.",
+    statOne: "Armor reduces the character's damage taken.",
+    statTwo: "Crit affects the character's Crit Rate. DMG increases when you Crit. (while confronting an enemy of your level)",
+    statThree: "Tenacity lowers the chance of being Critically hit and Crit. Dmg taken (while confronting an enemy of your level)",
+  }
+]
+
+export let hunterAttributes = [
+  {
+    name: "Agility",
+    desc: "Agility affects the ATK, HIT and Impale stats.",
+    statOne: "ATK affects the character's ATK DMG.",
+    statTwo: "HIT affects the character's Hit Rate (while confronting an enemy of your level, the primary hit rate is 95%)",
+    statThree: "Impale lowers the chance of your atttack being blocked and increases Crit DMG (while confronting an enemy of your level",
+  },
+  {
+    name: "Sta",
+    desc: "Stamina affects the HP, Block and Evasion stats.",
+    statOne: "HP affects the character's Max HP. At 0 HP, the character dies.",
+    statTwo: "Block affects the character's Block Chance. DMG received is reduced when an attack is blocked (while confronting an enemy of your level)",
+    statThree: "Evasion affects the character's Evasion Rate. No damage is taken when an attack is evaded (while confronting an enemy of your level)"
+  },
+  {
+    name: "Tech",
+    desc: "Tech affects the Armor, Crit and Tenacity stats.",
+    statOne: "Armor reduces the character's damage taken.",
+    statTwo: "Crit affects the character's Crit Rate. DMG increases when you Crit. (while confronting an enemy of your level)",
+    statThree: "Tenacity lowers the chance of being Critically hit and Crit. Dmg taken (while confronting an enemy of your level)",
+  }
+]
+
+export let druidAttributes = [
+  {
+    name: "Int",
+    desc: "Int affects the ATK, HIT and Impale stats.",
+    statOne: "ATK affects the character's ATK DMG.",
+    statTwo: "HIT affects the character's Hit Rate (while confronting an enemy of your level, the primary hit rate is 95%)",
+    statThree: "Impale lowers the chance of your atttack being blocked and increases Crit DMG (while confronting an enemy of your level",
+  },
+  {
+    name: "Sta",
+    desc: "Stamina affects the HP, Block and Evasion stats.",
+    statOne: "HP affects the character's Max HP. At 0 HP, the character dies.",
+    statTwo: "Block affects the character's Block Chance. DMG received is reduced when an attack is blocked (while confronting an enemy of your level)",
+    statThree: "Evasion affects the character's Evasion Rate. No damage is taken when an attack is evaded (while confronting an enemy of your level)"
+  },
+  {
+    name: "Tech",
+    desc: "Tech affects the Armor, Crit and Tenacity stats.",
+    statOne: "Armor reduces the character's damage taken.",
+    statTwo: "Crit affects the character's Crit Rate. DMG increases when you Crit. (while confronting an enemy of your level)",
+    statThree: "Tenacity lowers the chance of being Critically hit and Crit. Dmg taken (while confronting an enemy of your level)",
+  }
+]
+
+export let mageAttributes = [
+  {
+    name: "Int",
+    desc: "Int affects the ATK, HIT and Impale stats.",
+    statOne: "ATK affects the character's ATK DMG.",
+    statTwo: "HIT affects the character's Hit Rate (while confronting an enemy of your level, the primary hit rate is 95%)",
+    statThree: "Impale lowers the chance of your atttack being blocked and increases Crit DMG (while confronting an enemy of your level",
+  },
+  {
+    name: "Sta",
+    desc: "Stamina affects the HP, Block and Evasion stats.",
+    statOne: "HP affects the character's Max HP. At 0 HP, the character dies.",
+    statTwo: "Block affects the character's Block Chance. DMG received is reduced when an attack is blocked (while confronting an enemy of your level)",
+    statThree: "Evasion affects the character's Evasion Rate. No damage is taken when an attack is evaded (while confronting an enemy of your level)"
+  },
+  {
+    name: "Tech",
+    desc: "Tech affects the Armor, Crit and Tenacity stats.",
+    statOne: "Armor reduces the character's damage taken.",
+    statTwo: "Crit affects the character's Crit Rate. DMG increases when you Crit. (while confronting an enemy of your level)",
+    statThree: "Tenacity lowers the chance of being Critically hit and Crit. Dmg taken (while confronting an enemy of your level)",
+  }
+]
+
+export let shamanAttributes = [
+  {
+    name: "Int",
+    desc: "Int affects the ATK, HIT and Impale stats.",
+    statOne: "ATK affects the character's ATK DMG.",
+    statTwo: "HIT affects the character's Hit Rate (while confronting an enemy of your level, the primary hit rate is 95%)",
+    statThree: "Impale lowers the chance of your atttack being blocked and increases Crit DMG (while confronting an enemy of your level",
+  },
+  {
+    name: "Sta",
+    desc: "Stamina affects the HP, Block and Evasion stats.",
+    statOne: "HP affects the character's Max HP. At 0 HP, the character dies.",
+    statTwo: "Block affects the character's Block Chance. DMG received is reduced when an attack is blocked (while confronting an enemy of your level)",
+    statThree: "Evasion affects the character's Evasion Rate. No damage is taken when an attack is evaded (while confronting an enemy of your level)"
+  },
+  {
+    name: "Tech",
+    desc: "Tech affects the Armor, Crit and Tenacity stats.",
+    statOne: "Armor reduces the character's damage taken.",
+    statTwo: "Crit affects the character's Crit Rate. DMG increases when you Crit. (while confronting an enemy of your level)",
+    statThree: "Tenacity lowers the chance of being Critically hit and Crit. Dmg taken (while confronting an enemy of your level)",
+  }
+]
+
+export let warlockAttributes = [
+  {
+    name: "Int",
+    desc: "Int affects the ATK, HIT and Impale stats.",
+    statOne: "ATK affects the character's ATK DMG.",
+    statTwo: "HIT affects the character's Hit Rate (while confronting an enemy of your level, the primary hit rate is 95%)",
+    statThree: "Impale lowers the chance of your atttack being blocked and increases Crit DMG (while confronting an enemy of your level",
+  },
+  {
+    name: "Sta",
+    desc: "Stamina affects the HP, Block and Evasion stats.",
+    statOne: "HP affects the character's Max HP. At 0 HP, the character dies.",
+    statTwo: "Block affects the character's Block Chance. DMG received is reduced when an attack is blocked (while confronting an enemy of your level)",
+    statThree: "Evasion affects the character's Evasion Rate. No damage is taken when an attack is evaded (while confronting an enemy of your level)"
+  },
+  {
+    name: "Tech",
+    desc: "Tech affects the Armor, Crit and Tenacity stats.",
+    statOne: "Armor reduces the character's damage taken.",
+    statTwo: "Crit affects the character's Crit Rate. DMG increases when you Crit. (while confronting an enemy of your level)",
+    statThree: "Tenacity lowers the chance of being Critically hit and Crit. Dmg taken (while confronting an enemy of your level)",
+  }
+]
+
+export let gladAttributes = [
+  {
+    name: "Str",
+    desc: "Str affects the ATK, HIT and Impale stats.",
+    statOne: "ATK affects the character's ATK DMG.",
+    statTwo: "HIT affects the character's Hit Rate (while confronting an enemy of your level, the primary hit rate is 95%)",
+    statThree: "Impale lowers the chance of your atttack being blocked and increases Crit DMG (while confronting an enemy of your level",
+  },
+  {
+    name: "Sta",
+    desc: "Stamina affects the HP, Block and Evasion stats.",
+    statOne: "HP affects the character's Max HP. At 0 HP, the character dies.",
+    statTwo: "Block affects the character's Block Chance. DMG received is reduced when an attack is blocked (while confronting an enemy of your level)",
+    statThree: "Evasion affects the character's Evasion Rate. No damage is taken when an attack is evaded (while confronting an enemy of your level)"
+  },
+  {
+    name: "Tech",
+    desc: "Tech affects the Armor, Crit and Tenacity stats.",
+    statOne: "Armor reduces the character's damage taken.",
+    statTwo: "Crit affects the character's Crit Rate. DMG increases when you Crit. (while confronting an enemy of your level)",
+    statThree: "Tenacity lowers the chance of being Critically hit and Crit. Dmg taken (while confronting an enemy of your level)",
+  }
+]
+
+export let warriorAttributes = [
+  {
+    name: "Str",
+    desc: "Str affects the ATK, HIT and Impale stats.",
+    statOne: "ATK affects the character's ATK DMG.",
+    statTwo: "HIT affects the character's Hit Rate (while confronting an enemy of your level, the primary hit rate is 95%)",
+    statThree: "Impale lowers the chance of your atttack being blocked and increases Crit DMG (while confronting an enemy of your level",
+  },
+  {
+    name: "Sta",
+    desc: "Stamina affects the HP, Block and Evasion stats.",
+    statOne: "HP affects the character's Max HP. At 0 HP, the character dies.",
+    statTwo: "Block affects the character's Block Chance. DMG received is reduced when an attack is blocked (while confronting an enemy of your level)",
+    statThree: "Evasion affects the character's Evasion Rate. No damage is taken when an attack is evaded (while confronting an enemy of your level)"
+  },
+  {
+    name: "Tech",
+    desc: "Tech affects the Armor, Crit and Tenacity stats.",
+    statOne: "Armor reduces the character's damage taken.",
+    statTwo: "Crit affects the character's Crit Rate. DMG increases when you Crit. (while confronting an enemy of your level)",
+    statThree: "Tenacity lowers the chance of being Critically hit and Crit. Dmg taken (while confronting an enemy of your level)",
+  }
+]
