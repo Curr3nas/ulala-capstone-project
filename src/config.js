fba20555c2d7139b7cca6a46aefc600f86ef6cd8
Douglas Miner <curr3nas@gmail.com>
2020-04-24 14:24:31 -0500

    think i fixed the communication problems

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'https://murmuring-hollows-51888.herokuapp.com/',
+  API_ENDPOINT: 'https://murmuring-hollows-51888.herokuapp.com',
   TOKEN_KEY: 'ulala-guide-auth-token'
 }
